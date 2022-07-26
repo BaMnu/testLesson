cba6a25135c60c3d516b2bf890bd681bff143d7d
Olga Fedorova <bamnu13@gmail.com>
2022-07-26 18:05:37 +0300

    Added new Strings

--- TestTest/src/Main.java
+++ TestTest/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String hi = "Hello!";
+        String w = "World!";
+        System.out.println(hi + " " + w);
     }
 }
